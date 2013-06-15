bbfaa8511d4c4cad8dfc7045677950ae2c3dd91b
Lucas <metallucas.ds@gmail.com>
2013-06-15 10:41:24 -0300

    Update app.js

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+(function(exports, $, bb){
+    $(function(){
+        var $window, $body, $document;
+
+        $window = $(window);
+        $document = $(document);
+        $body = $('body');
+    });
+}(this, jQuery, Backbone));
