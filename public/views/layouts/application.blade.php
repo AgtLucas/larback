9f76126ce37d6a79a0a21726f4280adfa293ea56
Lucas <metallucas.ds@gmail.com>
2013-06-15 10:56:39 -0300

    Update application layout

--- public/views/layouts/application.blade.php
+++ public/views/layouts/application.blade.php
@@ -83,7 +83,7 @@
      <span class="icon-bar"></span>
      <span class="icon-bar"></span>
     </button>
-    <a class="brand" href="#">Nettuts Tutorial</a>
+    <a class="brand" href="#">Laravel & Backbone</a>
     <div class="nav-collapse collapse">
      <ul class="nav">
       <li class="active"><a href="#">Blog</a></li>
